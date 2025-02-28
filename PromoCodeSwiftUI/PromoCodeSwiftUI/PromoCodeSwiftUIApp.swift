ddd9902cdb53ef3d3072303743769c41e456de9e
SanekWhite <bro.b98@mail.ru>
2025-02-28 12:16:11 +0700

    Created Project

---
+++ PromoCodeSwiftUI/PromoCodeSwiftUI/PromoCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromoCodeSwiftUIApp.swift
+//  PromoCodeSwiftUI
+//
+//  Created by Александр Белый on 28.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PromoCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
